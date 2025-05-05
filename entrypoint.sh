a19093782c55a26a11f1e22675fb6f283589bcee
Kiril Tsanov <46856249+KikoTs@users.noreply.github.com>
2025-05-05 19:52:14 +0300

    More filters

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,5 +23,25 @@ export DB_POSTGRESDB_USER=$N8N_DB_USER
 export DB_POSTGRESDB_PASSWORD=$N8N_DB_PASSWORD
 
 sed -i.bak -E 's#this\.manager\?\.[[:space:]]*hasFeatureEnabled\(feature\)[[:space:]]*\?\?[[:space:]]*false#true#g' /usr/local/lib/node_modules/n8n/dist/license.js
+sed -i.bak '/getConsumerId()/,/}/ s#return.*;#        return '\''123-;D'\'';#' /usr/local/lib/node_modules/n8n/dist/license.js
+sed -i.bak '/getUsersLimit()/,/}/    s#return.*;#        return constants_1.UNLIMITED_LICENSE_QUOTA;#' /usr/local/lib/node_modules/n8n/dist/license.js
+sed -i.bak '/getTriggerLimit()/,/}/   s#return.*;#        return constants_1.UNLIMITED_LICENSE_QUOTA;#' /usr/local/lib/node_modules/n8n/dist/license.js
+sed -i.bak '/getVariablesLimit()/,/}/ s#return.*;#        return constants_1.UNLIMITED_LICENSE_QUOTA;#' /usr/local/lib/node_modules/n8n/dist/license.js
+sed -i.bak '/getAiCredits()/,/}/       s#return.*;#        return 999;#' /usr/local/lib/node_modules/n8n/dist/license.js
+sed -i.bak '/getWorkflowHistoryPruneLimit()/,/}/ s#return.*;#        return constants_1.UNLIMITED_LICENSE_QUOTA;#' /usr/local/lib/node_modules/n8n/dist/license.js
+sed -i.bak '/getInsightsMaxHistory()/,/}/        s#return.*;#        return 180;#' /usr/local/lib/node_modules/n8n/dist/license.js
+sed -i.bak '/getInsightsRetentionMaxAge()/,/}/   s#return.*;#        return 1800;#' /usr/local/lib/node_modules/n8n/dist/license.js
+sed -i.bak '/getInsightsRetentionPruneInterval()/,/}/ s#return.*;#        return 2400;#' /usr/local/lib/node_modules/n8n/dist/license.js
+sed -i.bak '/getTeamProjectLimit()/,/}/      s#return.*;#        return 200;#' /usr/local/lib/node_modules/n8n/dist/license.js
+sed -i.bak -E "s#this\.getFeatureValue\('planName'\)[[:space:]]*\?\?[[:space:]]*'Community';#'Enterprise';#g" /usr/local/lib/node_modules/n8n/dist/license.js
+
+sed -i.bak 's#await this\.manager\.activate(activationKey);#//await this.manager.activate(activationKey);#g' /usr/local/lib/node_modules/n8n/dist/license.js
+sed -i.bak 's#await this\.manager\.reload();#//await this.manager.reload();#g' /usr/local/lib/node_modules/n8n/dist/license.js
+sed -i.bak 's#await this\.manager\.clear();#//await this.manager.clear();#g' /usr/local/lib/node_modules/n8n/dist/license.js
+sed -i.bak 's#await this\.manager\.renew();#//await this.manager.renew();#g' /usr/local/lib/node_modules/n8n/dist/license.js
+sed -i.bak 's#await this\.manager\.shutdown();#//await this.manager.shutdown();#g' /usr/local/lib/node_modules/n8n/dist/license.js
+
+
+
 # kickstart nodemation
 n8n
