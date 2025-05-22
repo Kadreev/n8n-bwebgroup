423a06ce3e8e469de942db89d563b114e3718d48
Kiril Tsanov <46856249+KikoTs@users.noreply.github.com>
2025-05-22 15:26:50 +0300

    Fixed updated namings for validation

--- entrypoint.sh
+++ entrypoint.sh
@@ -35,7 +35,7 @@ sed -i.bak '/isAPIDisabled()/,/}/        s#return.*;#        return false;#' /us
 sed -i.bak '/getInsightsRetentionMaxAge()/,/}/   s#return.*;#        return 1800;#' /usr/local/lib/node_modules/n8n/dist/license.js
 sed -i.bak '/getInsightsRetentionPruneInterval()/,/}/ s#return.*;#        return 2400;#' /usr/local/lib/node_modules/n8n/dist/license.js
 sed -i.bak '/getTeamProjectLimit()/,/}/      s#return.*;#        return 200;#' /usr/local/lib/node_modules/n8n/dist/license.js
-sed -i.bak -E "s#this\.getFeatureValue\('planName'\)[[:space:]]*\?\?[[:space:]]*'Community';#'Enterprise';#g" /usr/local/lib/node_modules/n8n/dist/license.js
+sed -i.bak -E "s#this\.getValue\('planName'\)[[:space:]]*\?\?[[:space:]]*'Community';#'Enterprise';#g" /usr/local/lib/node_modules/n8n/dist/license.js
 
 sed -i.bak 's#await this\.manager\.activate(activationKey);#//await this.manager.activate(activationKey);#g' /usr/local/lib/node_modules/n8n/dist/license.js
 sed -i.bak 's#await this\.manager\.reload();#//await this.manager.reload();#g' /usr/local/lib/node_modules/n8n/dist/license.js
@@ -43,7 +43,7 @@ sed -i.bak 's#await this\.manager\.clear();#//await this.manager.clear();#g' /us
 sed -i.bak 's#await this\.manager\.renew();#//await this.manager.renew();#g' /usr/local/lib/node_modules/n8n/dist/license.js
 sed -i.bak 's#await this\.manager\.shutdown();#//await this.manager.shutdown();#g' /usr/local/lib/node_modules/n8n/dist/license.js
 
-sed -i.bak 's#showNonProdBanner: this\.license\.isFeatureEnabled(constants_1\.LICENSE_FEATURES\.SHOW_NON_PROD_BANNER),#showNonProdBanner: false,#g' /usr/local/lib/node_modules/n8n/dist/services/frontend.service.js
+sed -i.bak 's#showNonProdBanner: this\.license\.isLicensed(constants_1\.LICENSE_FEATURES\.SHOW_NON_PROD_BANNER),#showNonProdBanner: false,#g' /usr/local/lib/node_modules/n8n/dist/services/frontend.service.js
 
 
 # kickstart nodemation
